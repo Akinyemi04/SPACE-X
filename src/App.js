c1487fcc6ce87de8408ea1337047d293b69d11d6
unknown <gabrielayoku@gmail.com>
2022-06-07 16:06:26 +0100

    initial code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
 import './App.css';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Technology from './Component/Technology';
+import Crew from './Component/Crew';
 
+import Home from './Component/Home';
+import Destination from './Component/Destination';
 function App() {
+  const[king,setKing]=useState()
+  useEffect(()=>{
+    fetch('data.json')
+    .then(res=>(res.json()))
+    .then(data=>setKing(data))
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        { king &&
+        <>        
+          <Routes>
+            <Route exact path='/' element={<Home/>}>
+            </Route>
+            <Route path='/destination' element={<Destination data={king}/>}>
+            </Route>
+            <Route path='/technology' element={<Technology data={king} />}>
+            </Route>
+            <Route path='/crew'  element={<Crew data={king}/>}></Route>
+          </Routes> 
+        </>}
+          
+      </div>
+    </BrowserRouter>
   );
 }
 
